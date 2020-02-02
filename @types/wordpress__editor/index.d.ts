f43364cbe485b4c584f4467b7bfbf9155a3a3196
lipemat <mat@matlipe.com>
2020-02-01 23:27:44 -0500

    Add types for initializing the classic editor (tinymce)

--- @types/wordpress__editor/index.d.ts
+++ @types/wordpress__editor/index.d.ts
@@ -2,6 +2,13 @@ declare module '@wordpress/editor' {
 	import {ComponentType} from 'react';
 	import {withInstanceIdProps} from '@wordpress/compose';
 	import {withSpokenMessages} from '@wordpress/components'
+	import {Settings} from 'tinymce';
+
+	/**
+	 * Initialize the classic tinymce editor.
+	 * @link https://github.com/WordPress/gutenberg/blob/master/packages/block-library/src/classic/edit.js
+	 */
+	type initialize = ( id: string, settings: Settings ) => void;
 
 
 	/**
@@ -26,8 +33,10 @@ declare module '@wordpress/editor' {
 	}
 
 	export const HierarchicalTermSelector: ComponentType<HierarchicalTermSelector>;
+	export const initialize: initialize;
 
 	export default interface Editor {
+		initialize: initialize
 		HierarchicalTermSelector: ComponentType<HierarchicalTermSelector>;
 	}
 }
