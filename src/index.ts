e44afc4c913fef3c7c2d7927c9cff40a70cd565d
lipemat <mat@matlipe.com>
2020-06-18 14:22:41 -0600

    Support providing a nonce for external requests
    Individual method for setting a nonce, and a parameter added to
    setRootURL which allows setting it there.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './wpapi';
 export {default as wpapi} from './wpapi';
-export {clearNonce, restoreNonce, setRootURL} from './util/root-url';
+export {setRootURL} from './util/root-url';
 export {addMiddleware, removeMiddleware, defaultFetchHandler} from './util/request-handler';
 export * from './util/authorize';
+export {restoreNonce, clearNonce, hasExternalNonce} from './util/nonce';
