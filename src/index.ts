7e4f608df5464e8451a5e84a550398a5df43a1f4
lipemat <mat@matlipe.com>
2020-06-18 14:39:42 -0600

    Expose the `setNonce` function.

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export {default as wpapi} from './wpapi';
 export {setRootURL} from './util/root-url';
 export {addMiddleware, removeMiddleware, defaultFetchHandler} from './util/request-handler';
 export * from './util/authorize';
-export {restoreNonce, clearNonce, hasExternalNonce} from './util/nonce';
+export {setNonce, restoreNonce, clearNonce, hasExternalNonce} from './util/nonce';
