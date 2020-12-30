169f89e0601c7d149cd2cfe90cb739ef7a3d064d
lipemat <mat@matlipe.com>
2020-12-30 14:14:47 -0700

    Expose `restoreRootURL` to external libraries via index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './wpapi';
 export {default as wpapi} from './wpapi';
-export {setRootURL} from './util/root-url';
+export {setRootURL, restoreRootURL} from './util/root-url';
 export {defaultFetchHandler} from './util/request-handler';
 export {addMiddleware, removeMiddleware} from './util/middleware';
 export * from './util/authorize';
