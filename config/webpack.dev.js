8783166357078b1ef959142e68a2b58e8950ec59
lipemat <mat@matlipe.com>
2021-08-12 15:50:37 -0400

    Support CSS module styles in Gutenberg iFrames
    The FSE support for Gutenberg loads within an iframe. We must target the
     iframe specifically when generating styles via `style-loader` or styles
      won't work.
    
    Future area are also expected to be in iframes.
    
    @notice You must any block with master.css as it's `style` for it load
    when script debug is off.
    
    ```php
    register_block_type( 'lipe-project/master', [
    				'editor_style' => self::ADMIN_HANDLE . '-css',
    			] );
    ```

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,34 @@
 const webpackConfig = require( '@lipemat/js-boilerplate/config/webpack.dev' );
 const wpExternals = require( '../helpers/wp-externals' );
-let externalsDefault = Object.assign( {}, webpackConfig.externals );
+const externalsDefault = Object.assign( {}, webpackConfig.externals );
+const rules = Object.assign( {}, webpackConfig.module.rules );
+
+/**
+ * Gutenberg is loading within FSE and future areas within an iFrame.
+ * We must target said iFrame with the generated <style> tags or
+ * our styles won't show up in the editor.
+ *
+ * 1. We must wait for the iframe to load as it's generated via JS.
+ *    so we can't simply point `insert` to `[name="editor-canvas"]`.
+ * 2. We don't have much to go on except the iframe name :-(.
+ */
+rules[ 2 ].use[ 0 ] = {
+	loader: 'style-loader',
+	options: {
+		insert: styleTag => {
+			setTimeout( () => {
+				const gutenbergEditor = document.querySelector( 'iframe[name="editor-canvas"]' );
+				if ( gutenbergEditor ) {
+					gutenbergEditor.contentDocument.head.appendChild( styleTag );
+				} else {
+					// Default behavior.
+					document.querySelector( 'head' ).appendChild( styleTag );
+				}
+			}, 2000 );
+		},
+	},
+};
+
 
 module.exports = {
 	externals: {...externalsDefault, ...wpExternals},
