c30cb12b290a36149aab014de36c0b5f7a500dea
lipemat <mat@matlipe.com>
2023-04-26 16:43:27 -0500

    Split the iframeLoader into a helper and fix it for JS Boilerplate 9.1+
    Has been broken for a while because the index number changed on JS
    Boilerplate.
    
    - Split to external config so the webpack.dev config is easier to
    maintain.
    - Check for the correct /.pcss/ match before assuming we know the index.

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,97 +1,19 @@
-const webpackConfig = require( '@lipemat/js-boilerplate/config/webpack.dev' );
 const wpExternals = require( '../helpers/wp-externals' );
-const externalsDefault = Object.assign( {}, webpackConfig.externals );
-const rules = Object.assign( {}, webpackConfig.module.rules );
+const iframeLoader = require( '../helpers/iframe-loader' );
 
-// @todo Switch to function based return so don't need to require webpack.dist.
+module.exports = function( config ) {
+	/**
+	 * Change the loader to our custom loader
+	 * for the `/\.pcss$/` rule.
+	 */
+	config.module.rules.forEach( rule => {
+		if ( rule.test.test( '.pcss' ) ) {
+			rule.use[ 0 ] = iframeLoader;
+		}
+	} );
 
-/**
- * Gutenberg is loading within FSE and future areas within an iFrame.
- * We must target said iFrame with the generated <style> tags or
- * our styles won't show up in the editor.
- *
- * 1. We must wait for the iframe to load as it's generated via JS.
- *    so we can't simply point `insert` to `[name="editor-canvas"]`.
- * 2. We don't have much to go on except the iframe name :-(.
- *
- * @notice In order for styles to work in FSE when script debug is off
- *         you must register the style for the block using the CSS handle.
- *         Also, `wp_register_style` must be called before `get_block_editor_settings`
- *         is called so likely the `init` or `wp_loaded` action.
- *         ```php
- *          register_block_type( 'lipe-project/master', [
- *              'editor_style' => self::CSS_HANDLE,
- *          ] );
- *          ```
- *
- * @link https://developer.wordpress.org/block-editor/reference-guides/block-api/block-metadata/#wpdefinedasset
- */
-rules[ 2 ].use[ 0 ] = {
-	loader: 'style-loader',
-	options: {
-		attributes: {
-			name: 'style-loader',
-		},
-		styleTagTransform: ( content, el ) => {
-			/**
-			 * Taken verbatim from style-loader.
-			 *
-			 * Must live inside this arrow function to be included in
-			 * the browser.
-			 *
-			 * @link https://github.dev/webpack-contrib/style-loader/blob/master/src/runtime/styleTagTransform.js
-			 */
-			// eslint-disable-next-line no-shadow
-			function styleTagTransform( css, styleElement ) {
-				if ( styleElement.styleSheet ) {
-					styleElement.styleSheet.cssText = css;
-				} else {
-					while ( styleElement.firstChild ) {
-						styleElement.removeChild( styleElement.firstChild );
-					}
-
-					styleElement.appendChild( document.createTextNode( css ) );
-				}
-			}
-
-			function cloneToGutenbergIframe() {
-				const gutenbergEditor = document.querySelector( 'iframe[name="editor-canvas"]' );
-				if ( gutenbergEditor ) {
-					// Store the cloned style tag on property for reuse.
-					el.iframeCloned = el.cloneNode( true );
-					gutenbergEditor.contentDocument.head.appendChild( el.iframeCloned );
-				}
-			}
-
-			// Default transformation of <style> tag on the root document.
-			styleTagTransform( content, el );
-
-			// Duplicate style tag on Gutenberg iframe and transform.
-			if ( ! el.iframeCloned ) {
-				setTimeout( () => {
-					cloneToGutenbergIframe();
-					// Try again in a few seconds...
-					if ( ! el.iframeCloned ) {
-						setTimeout( () => {
-							cloneToGutenbergIframe();
-							if ( ! el.iframeCloned ) {
-								// Use `no-iframe-available` to prevent checking for the iframe on every change.
-								el.iframeCloned = 'no-iframe-available';
-							}
-						}, 5000 );
-					}
-				}, 2000 );
-
-				// Transform the iframe's <style> tag.
-			} else if ( el.iframeCloned && 'no-iframe-available' !== el.iframeCloned ) {
-				styleTagTransform( content, el.iframeCloned );
-			}
-		},
-	},
-};
-
-
-module.exports = {
-	// Add the global `wp` variable based externals.
-	externals: {...externalsDefault, ...wpExternals},
+	return {
+		// Add the global `wp` variable based externals.
+		externals: {...config.externals, ...wpExternals},
+	};
 };
---
+++ helpers/iframe-loader.js
@@ -0,0 +1,73 @@
+/**
+ * Gutenberg is loading within FSE and future areas within an iFrame.
+ * We must target said iFrame with the generated <style> tags, or
+ * our styles won't show up in the editor.
+ *
+ * 1. We must wait for the iframe to load as it's generated via JS,
+ *    so we can't simply point `insert` to `[name="editor-canvas"]`.
+ * 2. Not much to go on except the iframe name :-(.
+ */
+const iframeLoader = {
+	loader: 'style-loader',
+	options: {
+		attributes: {
+			// Add an attribute to the `<style name="style-loader">` to identify it.
+			name: 'style-loader',
+		},
+		styleTagTransform: ( content, el ) => {
+			/**
+			 * Taken verbatim from style-loader.
+			 *
+			 * Must live inside this arrow function to be included in
+			 * the browser.
+			 *
+			 * @link https://github.dev/webpack-contrib/style-loader/blob/master/src/runtime/styleTagTransform.js
+			 */
+			function styleTagTransform( css, styleElement ) {
+				if ( styleElement.styleSheet ) {
+					styleElement.styleSheet.cssText = css;
+				} else {
+					while ( styleElement.firstChild ) {
+						styleElement.removeChild( styleElement.firstChild );
+					}
+
+					styleElement.appendChild( document.createTextNode( css ) );
+				}
+			}
+
+			function cloneToGutenbergIframe() {
+				const gutenbergEditor = document.querySelector( 'iframe[name="editor-canvas"]' );
+				if ( gutenbergEditor ) {
+					// Store the cloned style tag on property for reuse.
+					el.iframeCloned = el.cloneNode( true );
+					gutenbergEditor.contentDocument.head.appendChild( el.iframeCloned );
+				}
+			}
+
+			// Default transformation of <style> tag on the root document.
+			styleTagTransform( content, el );
+			// Duplicate style tag on Gutenberg iframe and transform.
+			if ( ! el.iframeCloned ) {
+				setTimeout( () => {
+					cloneToGutenbergIframe();
+					// Try again in a few seconds...
+					if ( ! el.iframeCloned ) {
+						setTimeout( () => {
+							cloneToGutenbergIframe();
+							if ( ! el.iframeCloned ) {
+								// Use `no-iframe-available` to prevent checking for the iframe on every change.
+								el.iframeCloned = 'no-iframe-available';
+							}
+						}, 5000 );
+					}
+				}, 2000 );
+
+				// Transform the iframe's <style> tag.
+			} else if ( el.iframeCloned && 'no-iframe-available' !== el.iframeCloned ) {
+				styleTagTransform( content, el.iframeCloned );
+			}
+		},
+	},
+};
+
+module.exports = iframeLoader;
